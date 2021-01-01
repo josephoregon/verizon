e4eb617449eaf533e7ee2bf522861547cd568410
josephoregon <josephoregon@icloud.com>
2021-01-01 17:08:42 -0600

    Update verizon_bill.py

--- verizon_bill.py
+++ verizon_bill.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+import altair as alt
+
 def get_data():
-  df = pd.read_csv('VERIZON_BILL_CSV.csv')
   
+  df = pd.read_csv('VERIZON_BILL_CSV.csv')
+
   df['Charge'] = df['Charge'].str.replace('\t', '')
   df['Charge'] = df['Charge'].str.replace('$', '')
   df['Charge'] = df['Charge'].str.replace('(', '-')
   df['Charge'] = df['Charge'].str.replace(')', '')
-  
+
   df['Charge'] = pd.to_numeric(df['Charge'])
 
   return df
@@ -20,5 +23,7 @@ def main():
   df = get_data()
   
   df.groupby('Person').Charge.sum().sort_values()
+  
+  st.bar_chart(df)
   
 main()
