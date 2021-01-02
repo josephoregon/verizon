a24c88be973e5eb04c05fe6f51af5d5401d497f8
josephoregon <josephoregon@icloud.com>
2021-01-01 21:24:33 -0600

    Update verizon_bill.py

--- verizon_bill.py
+++ verizon_bill.py
@@ -41,16 +41,15 @@ def main():
     st.markdown('---')
     
     grouped_df = df.groupby(graph_list)['Charge Amount'].sum().sort_values()
-    #grouped_df = grouped_df.rename('Charge Amount').reset_index()
+    grouped_df = grouped_df.rename('Charge Amount')#.reset_index()
     grouped_df['Charge Amount'] = grouped_df['Charge Amount'].map('${:,.2f}'.format)
     
     
-    fig = \
-        go.Figure(data=[go.Table(header=dict(values=list(grouped_df.columns),
-                  fill_color='light blue', align='center'),
-                  cells=dict(values=[grouped_df.Person,
-                  grouped_df['Charge Amount']], fill_color='light gray',
-                  align='center'))])
+    fig = go.Figure(data=[go.Table(header=dict(values=list(grouped_df.columns),
+                          fill_color='light blue', align='center'),
+                          cells=dict(values=[grouped_df.Person,
+                          grouped_df['Charge Amount']], fill_color='light gray',
+                          align='center'))])
 
     fig.update_layout(width=600, height=400)
     
