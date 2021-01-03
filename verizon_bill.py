0db99471f788056dc6e96db3caa6a6d4be161ea4
josephoregon <josephoregon@icloud.com>
2021-01-03 17:47:58 -0600

    Update verizon_bill.py

--- verizon_bill.py
+++ verizon_bill.py
@@ -8,6 +8,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly import tools
 
+st.set_page_config(
+            
+            page_title="Ex-stream-ly Cool App",
+            page_icon="🧊",
+            layout="wide",
+            initial_sidebar_state="expanded"
+            
+      )
+
+
+
 st.markdown("<h1 style='text-align: center;'>Eckhardt Verizon</h1>"
             , unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>Breakdown of the Verizon billing.</h3>"
